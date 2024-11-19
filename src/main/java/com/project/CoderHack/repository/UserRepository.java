5dabbd12c98faf43fc11e172f9cdbaf020d8b0b6
Anshu Geetanjali <ageetanjali308@gmail.com>
2024-11-20 01:10:19 +0530

    Add Repository layer

---
+++ src/main/java/com/project/CoderHack/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.CoderHack.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.CoderHack.entity.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+
+    User findByUsername(String username);
+    void deleteById(String id);
+}
